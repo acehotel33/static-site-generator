e49d3e19e48d04073b190bedd0ba2bedbe22ac7d
AceHotel33 <lomvax@gmail.com>
2024-08-06 14:02:20 +0400

    Added split_nodes_delimiter and unit tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,12 @@
 import unittest
 
-from textnode import TextNode
+from textnode import TextNode, split_nodes_delimiter
+
+text_type_text="text"
+text_type_code="code"
+text_type_bold="bold"
+text_type_italic="italic"
+
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -23,6 +29,70 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", "bold")
         node2 = TextNode("This is a text node", "bold", "yo")
         self.assertNotEqual(node, node2)
+
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_code_split(self):
+        node = TextNode("`code block` at the start and `at the end`", text_type_text)        
+        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+        expected = [TextNode("code block", "code", None), TextNode(" at the start and ", "text", None), TextNode("at the end", "code", None)]
+        actual = new_nodes
+        self.assertEqual(expected, actual)
+
+    def test_code_split_multiple(self):
+        node1 = TextNode("`code block` at the start and `at the end`", text_type_text)        
+        node2 = TextNode("Now it's `in the middle` and this is just text", text_type_text)
+        node3 = TextNode("This is simply empty text", text_type_text)
+        new_nodes = split_nodes_delimiter([node1, node2, node3], "`", text_type_code)
+        expected = [
+            TextNode("code block", "code", None), 
+            TextNode(" at the start and ", "text", None), 
+            TextNode("at the end", "code", None),
+            TextNode("Now it's ", "text", None),
+            TextNode("in the middle", "code", None),
+            TextNode(" and this is just text", "text", None),
+            TextNode("This is simply empty text", "text", None)
+            ]
+        actual = new_nodes
+        self.assertEqual(expected, actual)
+
+    def test_code_split_only_text(self):
+        node = TextNode("Simply text", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+        expected = [TextNode("Simply text", "text", None)]
+        actual = new_nodes
+        self.assertEqual(expected, actual)
+
+    def test_bold_split(self):
+        node = TextNode("**Bold** at the start and **at the end**", text_type_text)        
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        expected = [TextNode("Bold", "bold", None), TextNode(" at the start and ", "text", None), TextNode("at the end", "bold", None)]
+        actual = new_nodes
+        self.assertEqual(expected, actual)  
+
+    def test_invalid_markdown_syntax(self):
+        node = TextNode("`code block at the start and  `at the end`", "text")
+        with self.assertRaises(Exception) as context:
+            split_nodes_delimiter([node], "`", "code")
+        self.assertEqual(str(context.exception), "Node has invalid Markdown syntax: missing opening or closing delimiter")
+
+    def test_non_text_nodes(self):
+        node = TextNode("This is a non-text node", "code")
+        new_nodes = split_nodes_delimiter([node], "`", "code")
+        expected = [node]
+        self.assertEqual(new_nodes, expected)
+
+
+# node1 = TextNode("`code block` at the start and `at the end`", text_type_text)
+# # new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+# # print(new_nodes)
+# node2 = TextNode("This is text with a `code block` word", text_type_text)
+# # new_nodes2 = split_nodes_delimiter([node2], "`",  text_type_code)
+
+# node3 = TextNode("This is a regular text", text_type_text)
+# new_nodes = split_nodes_delimiter([node1, node2, node3], "`", text_type_code)
+# print(new_nodes)
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -5,9 +5,22 @@ def main():
     example4 = TextNode('','')
     dummy = TextNode('This is a text node', 'bold', 'https://www.boot.dev')
 
-    print(example)
-    print(example2)
-    print(dummy)
+    node1 = TextNode("`code block` at the start and `at the end`", text_type_text)
+    # new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+    # print(new_nodes)
+    node2 = TextNode("This is text with a `code block` word", text_type_text)
+    # new_nodes2 = split_nodes_delimiter([node2], "`",  text_type_code)
+    
+    node3 = TextNode("This is a regular text", text_type_text)
+    new_nodes = split_nodes_delimiter([node1, node2, node3], "`", text_type_code)
+    print(new_nodes)
+
+
+text_type_text="text"
+text_type_code="code"
+text_type_bold="bold"
+text_type_italic="italic"
+
 
 
 class TextNode:
@@ -22,6 +35,29 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text.count(delimiter) % 2 != 0:
+            raise Exception('Node has invalid Markdown syntax: missing opening or closing delimiter')
+        if node.text_type != "text":
+            new_nodes.append(node)
+        else:    
+            split_text = node.text.split(delimiter)
+            identifier = []
+            nodes = []
+            for i in range(len(split_text)):
+                if i % 2 == 0:
+                    if split_text[i] != "":
+                        nodes.append(TextNode(split_text[i], text_type_text))
+                else:
+                    if split_text[i] != "": 
+                        nodes.append(TextNode(split_text[i], text_type))
+            new_nodes.extend(nodes) 
+    return new_nodes
+
+# ['', 'code block', ' at the start and ', 'at the end', '']
 
 if __name__ == "__main__":
     main()
