b0a6adb5cc92b0eaeabc1009708ac9e0f8213a03
AceHotel33 <lomvax@gmail.com>
2024-08-09 12:50:15 +0400

    Finished block_to_block_type with unit tests

--- src/blocks.py
+++ src/blocks.py
@@ -1,6 +1,10 @@
 def main():
     markdown = "# This is a heading\n\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n\n* This is the first list item in a list block\n* This is a list item\n* This is another list item"
-    block = '```code block```'
+    # block = "> This is line one.\n> This is line two\n> This is line three..."
+    # block = "* This is line one.\n- This is line two."
+    # block = "1. hello\n2. can you hear me\n3. i was wondering if after all these years you'd like to meet"
+    # block = "1. 2. 3. \n2. \n3. "
+    # block = "hello mate"
     print(block_to_block_type(block))
     # print(markdown_to_blocks(markdown))
 
@@ -23,13 +27,44 @@ def block_to_block_type(block):
                 header_num = i
                 break
         headers_list = ['none', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
-        if block[header_num] == ' ':
-           return headers_list[header_num]
+        if len(block) > header_num:
+            if block[header_num] == ' ':
+               return headers_list[header_num]
 
     # check for code type
     if block[:3] == '```' and block[:3] == block[-3:] and len(block) >= 6:
         return 'code'
-        
+    
+    split_lines = block.split('\n')
 
+    # check for quote block
+    if block[0] == '>':
+        is_quote = True
+        for line in split_lines:
+            if line[0] != '>':
+                is_quote = False
+        if is_quote:
+            return 'quote'
+
+    # check for unordered list
+    if block[0] in ['*', '-']:
+        is_unordered = True
+        for line in split_lines:
+            if not (line[:2] == '* ' or line[:2] == '- '):
+                is_unordered = False
+        if is_unordered:
+            return 'unordered list'
+
+    # check for ordered list
+    if block[:2] == "1.":
+        line_count = len(split_lines)
+        is_ordered = True
+        for i in range(line_count):
+            if split_lines[i][:3] != f'{i+1}. ':
+                is_ordered = False
+        if is_ordered:
+            return 'ordered list'
+
+    return 'normal paragraph'
 if __name__=="__main__":
     main()
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from blocks import markdown_to_blocks
+from blocks import markdown_to_blocks, block_to_block_type
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -14,3 +14,158 @@ class TestMarkdownToBlocks(unittest.TestCase):
         expected = ["# This is a heading", "This is a paragraph of text. It has some **bold** and *italic* words inside of it.", "* This is the first list item in a list block\n* This is a list item\n* This is another list item"]
         actual = markdown_to_blocks(markdown)
         self.assertEqual(actual, expected)
+
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_btbt_h1(self):
+        block = '# Heading 1'
+        expected = 'h1' 
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_h2(self):
+        block = '## Heading 2'
+        expected = 'h2' 
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_h3(self):
+        block = '### Heading 3'
+        expected = 'h3' 
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_h4(self):
+        block = '#### Heading 4'
+        expected = 'h4' 
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_h5(self):
+        block = '##### Heading 5'
+        expected = 'h5' 
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+    
+    def test_btbt_h6(self):
+        block = '###### Heading 6'
+        expected = 'h6' 
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_h6_space(self):
+        block = ' ###### Heading 6'
+        expected = 'normal paragraph' 
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_h_empty_nospace(self):
+        block = '###'
+        expected = 'normal paragraph' 
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_h_empty_withspace(self):
+        block = '###### '
+        expected = 'h6' 
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_code(self):
+        block = '```some code```'
+        expected = 'code' 
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_code_empty(self):
+        block = '``````'
+        expected = 'code' 
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_code_unfinished(self):
+        block = '`````'
+        expected = 'normal paragraph' 
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_code_space_end(self):
+        block = '`````` '
+        expected = 'normal paragraph' 
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_quote(self):
+        block = "> Quote line 1\n> Quote line 2\n> Quote line3"
+        expected = 'quote'
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_quote_no_space(self):
+        block = "> Quote line 1\n>Quote line 2\n> Quote line3"
+        expected = 'quote'
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_quote_missing_start(self):
+        block = " Quote line 1\n> Quote line 2\n> Quote line3"
+        expected = 'normal paragraph'
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_quote_empty(self):
+        block = ">\n>\n>"
+        expected = 'quote'
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_unordered(self):
+        block = "- Unordered line 1\n- Unordered line 2\n- Unordered line 3"
+        expected = 'unordered list'
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_unordered_no_space(self):
+        block = "- Unordered line 1\n-Unordered line 2\n- Unordered line 3"
+        expected = 'normal paragraph'
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_unordered_stars(self):
+        block = "* Unordered line 1\n* Unordered line 2\n* Unordered line 3"
+        expected = 'unordered list'
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_unordered_mixed(self):
+        block = "* Unordered line 1\n- Unordered line 2\n* Unordered line 3"
+        expected = 'unordered list'
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_ordered(self):
+        block = "1. Ordered line 1\n2. Ordered line 2\n3. Ordered line 3"
+        expected = 'ordered list'
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_ordered_no_space(self):
+        block = "1.Ordered line 1\n2. Ordered line 2\n3. Ordered line 3"
+        expected = 'normal paragraph'
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_ordered_strange(self):
+        block = "1. 2. 3. \n2. \n3. "
+        expected = 'ordered list'
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_btbt_normal_paragraph(self):
+        block = "hello mate"
+        expected = "normal paragraph"
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+if __name__ == "__main__":
+    unittest.main()
