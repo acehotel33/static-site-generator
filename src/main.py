92e5191184653df9751ec307a5a667f9742f422a
AceHotel33 <lomvax@gmail.com>
2024-08-13 14:47:29 +0400

    Fine tuned copying function to only copy over contents and not root of source

--- src/main.py
+++ src/main.py
@@ -3,14 +3,12 @@ import shutil
 
 def main():
     # print(os.path.exists(destination_dir))
-    print("Contents of source directory:")
-    print(os.listdir(source_dir))
-    print("Contents of destination directory:")
-    print(os.listdir(destination_dir))
-    delete_contents_of_dir(destination_dir)
-    print(os.listdir(destination_dir))
+    # print(f"Contents of source directory:\n{os.listdir(source_dir)}")
+    # print(f"Contents of destination directory:\n{os.listdir(destination_dir)}")
+    # delete_contents_of_dir(destination_dir)
+    # print(f"Contents of destination directory:\n{os.listdir(destination_dir)}")
     copy_source_contents_to_destination_dir(source_dir, destination_dir)
-    print(os.listdir(destination_dir))
+    # print(f"Contents of destination directory:\n{os.listdir(destination_dir)}")
 
 static_dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'static')
 public_dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'public')
@@ -33,15 +31,20 @@ def delete_contents_of_dir(dir_path):
         print(f"The directory {directory_path} does not exist")
 
 def copy_source_contents_to_destination_dir(source_path, destination_path):
-    if os.path.exists(source_path) and os.path.exists(destination_path):
+    if not os.path.exists(destination_path):
+        os.makedirs(destination_path)
+    if os.path.exists(source_path):
         if os.path.isfile(source_path) or os.path.islink(source_path):
             shutil.copy(source_path, destination_path)
         elif os.path.isdir(source_path):
-            new_destination_path = os.path.join(destination_path, os.path.basename(source_path))
-            os.makedirs(new_destination_path)
             for file in os.listdir(source_path):
-                file_path = os.path.join(source_path, file)
-                copy_source_contents_to_destination_dir(file_path, new_destination_path)
+                file_source_path = os.path.join(source_path, file)
+                file_destination_path = os.path.join(destination_path, file)
+
+                if os.path.isdir(file_source_path):
+                    copy_source_contents_to_destination_dir(file_source_path, file_destination_path)
+                else:
+                    shutil.copy(file_source_path, destination_path)
     else:
         print(f"One of two paths is invalid: {source_path} or {destination_path}")
 
