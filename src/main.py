2172f1b3d6c965931078870145ab10778e198f93
AceHotel33 <lomvax@gmail.com>
2024-08-13 14:24:30 +0400

    Added copy_source_contents_to_destination_dir

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 def main():
     # print(os.path.exists(destination_dir))
@@ -8,7 +9,8 @@ def main():
     print(os.listdir(destination_dir))
     delete_contents_of_dir(destination_dir)
     print(os.listdir(destination_dir))
-
+    copy_source_contents_to_destination_dir(source_dir, destination_dir)
+    print(os.listdir(destination_dir))
 
 static_dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'static')
 public_dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'public')
@@ -30,7 +32,18 @@ def delete_contents_of_dir(dir_path):
     else:
         print(f"The directory {directory_path} does not exist")
 
-
+def copy_source_contents_to_destination_dir(source_path, destination_path):
+    if os.path.exists(source_path) and os.path.exists(destination_path):
+        if os.path.isfile(source_path) or os.path.islink(source_path):
+            shutil.copy(source_path, destination_path)
+        elif os.path.isdir(source_path):
+            new_destination_path = os.path.join(destination_path, os.path.basename(source_path))
+            os.makedirs(new_destination_path)
+            for file in os.listdir(source_path):
+                file_path = os.path.join(source_path, file)
+                copy_source_contents_to_destination_dir(file_path, new_destination_path)
+    else:
+        print(f"One of two paths is invalid: {source_path} or {destination_path}")
 
 main()
 
